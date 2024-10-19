3b513cadd8d2cfbd4ebb3585f620a1fa766c33e1
Cleber-0101 <cleberbatista99@hotmail.com>
2024-10-19 19:07:13 -0300

    Criando uma calculadora e explorando a linguagem Dart

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -1,5 +1,62 @@
-import 'package:calculadora/calculadora.dart' as calculadora;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${calculadora.calculate()}!');
+void main(){
+ double numero1 = 0;
+ double numero2 = 0;
+ String operacao = "";
+
+
+ //usando o "!" estamos garantindo que o dado não seja nulo
+
+  void Soma(){
+    print(numero1 + numero2);
+  }
+
+  void Subtracao(){
+    print(numero1 - numero2);
+  }
+
+  void divisao(){
+    print(numero1 / numero2);
+  }
+
+  void multiplicacao(){
+    print(numero1 * numero2);
+  }
+   
+   //O que vamos checkar ?
+   String? entrada = stdin.readLineSync();
+
+    if(entrada != null){
+      if(entrada != ""){
+        numero1 = double.parse(entrada);
+      }
+      numero1 = double.parse(entrada);
+    }
+
+    entrada = stdin.readLineSync();
+    if(entrada != null){
+      numero2 = double.parse(entrada);
+    }
+
+    entrada = stdin.readLineSync();
+    if (entrada != null) {
+      operacao = entrada;
+    }
+
+  switch(operacao){
+    case "+":
+    Soma();
+
+    case "-":
+    Subtracao();
+
+    case "*":
+    multiplicacao();
+
+    case "/":
+    divisao();
+    break;
+  }
+
 }
